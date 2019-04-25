2dde5f330850ca07a55b8c971faa6bfc8e65af49
Garret Thompson <gthompson@rsglab.com>
2019-04-25 12:43:24 -0400

    Implement client/server

---
+++ capClient.py
@@ -0,0 +1,16 @@
+import grpc
+
+import defs_pb2_grpc as proto
+import defs_pb2 as pb
+
+def main():
+    channel = grpc.insecure_channel('localhost:9778')
+    stub = proto.FetchStub(channel)
+
+    while True:
+        line_in = input('> ')
+        capitalized = stub.Capitalize(pb.Payload(data=bytes(line_in, encoding='utf-8')))
+        print('< %s\n' % capitalized.data.decode('utf-8'))
+
+if __name__ == "__main__":
+    main()
