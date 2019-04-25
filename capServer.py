f6dc1f99d348788a688011d98dba449a2cd447f1
Garret Thompson <gthompson@rsglab.com>
2019-04-25 12:57:16 -0400

    Set up the interceptor

--- capServer.py
+++ capServer.py
@@ -9,18 +9,32 @@ from opencensus.trace.ext.gprc import server_interceptor
 from opencensus.trace.samplers import always_on
 from opencensus.trace.tracer import Tracer
 
+
 class CapitalizeServer(proto.FetchServicer):
     def __init__(self, *args, **kwargs):
         super(CapitalizeServer, self).__init__()
 
     def Capitalize(self, request, context):
-        return pb.Payload(data=request.data.upper())
+        tracer = Tracer(sampler=always_on.AlwaysOnSampler())
+        with tracer.span(name="Capitalize") as span:
+            data = request.data
+            span.add_annotation("Data in", len=len(data))
+            return pb.Payload(data=request.data.upper())
 
 
 def main():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    # Set up the gRPC integration/interceptor
+    tracer_interceptor = server_interceptor.OpenCensusServerInterceptor(
+        always_on.AlwaysOnSampler()
+    )
+
+    server = grpc.server(
+        futures.ThreadPoolExecutor(max_workers=10),
+        interceptors=(tracer_interceptor,),
+    )
+
     proto.add_FetchServicer_to_server(CapitalizeServer(), server)
-    server.add_insecure_port('[::]:9778')
+    server.add_insecure_port("[::]:9778")
     server.start()
 
     try:
@@ -28,6 +42,7 @@ def main():
             time.sleep(3600)
     except KeyboardInterrupt:
         server.stop(0)
+
 
 if __name__ == "__main__":
     main()
