f53a6f420b89cd044e791f3d325784e3e07208a4
Garret Thompson <gthompson@rsglab.com>
2019-04-25 12:58:40 -0400

    Python formatting

--- capClient.py
+++ capClient.py
@@ -3,14 +3,18 @@ import grpc
 import defs_pb2_grpc as proto
 import defs_pb2 as pb
 
+
 def main():
-    channel = grpc.insecure_channel('localhost:9778')
+    channel = grpc.insecure_channel("localhost:9778")
     stub = proto.FetchStub(channel)
 
     while True:
-        line_in = input('> ')
-        capitalized = stub.Capitalize(pb.Payload(data=bytes(line_in, encoding='utf-8')))
-        print('< %s\n' % capitalized.data.decode('utf-8'))
+        line_in = input("> ")
+        capitalized = stub.Capitalize(
+            pb.Payload(data=bytes(line_in, encoding="utf-8"))
+        )
+        print("< %s\n" % capitalized.data.decode("utf-8"))
+
 
 if __name__ == "__main__":
     main()
